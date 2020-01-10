502084974476598904a7fd011ccac9f7ae058549
abhishekkushwaha4u <thecrazycoderabhi@gmail.com>
2020-01-10 20:55:57 +0530

    changes

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import DepartmentView, department_view, Health
 from django.urls import path
 
 urlpatterns = [
-    path('',DepartmentView.as_view()),
-    # path('', department_view),
+    #path('',DepartmentView.as_view()),
+    path('', department_view),
     path('check/', Health.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -52,27 +52,29 @@ class DepartmentView(APIView):
             
     def post(self, request, *args, **kwargs):
         print(request.data)
-        department_serializer = UpoloaderrSerializer(data=request.data)
+        # department_serializer = UpoloaderrSerializer(data=request.data)
 
-        if department_serializer.is_valid():
-            department_serializer.save()
-            return Response(department_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(department_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # if department_serializer.is_valid():
+        #     department_serializer.save()
+        #     return Response(department_serializer.data, status=status.HTTP_201_CREATED)
+        # else:
+        #     return Response(department_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def department_view(request):
     if request.method == "POST":
         print(request.POST)
-        department_serializer = UpoloaderrSerializer(data=request.POST, file=request.FILES)
-    
-    if department_serializer.is_valid():
-            department_serializer.save()
-            return HttpResponse(department_serializer.data)
-    else:
-        return HttpResponse(department_serializer.errors)
+        author = request.POST.get("author")
+        coress_author = request.POST.get("coress_author")
+        email = request.POST.get("email")
+        alt_email = request.POST.get("alt_email")
+        phone = request.POST.get("phone")
+        phone_phoneCode = request.POST.get("phone_phoneCode")
+        file = request.FILES['file']
+        print("Successfully Printed all params")
+        return HttpResponse("Working")
 
-class Health(APIView):
+class Health(APIVew):
     def get(self):
         return Response({"status": "Hello World"})
 
