e1606eda6c6558f6feea537a202628b5bb522355
unknown-guy-1610 <rayanuthalas@gmail.com>
2020-01-10 22:25:50 +0530

    dbgfhc

--- core/views.py
+++ core/views.py
@@ -53,7 +53,12 @@ class DepartmentView(APIView):
         department_serializer = UpoloaderrSerializer(data=request.data)
         if department_serializer.is_valid():
             department_serializer.save()
-            return Response(department_serializer.data, status=status.HTTP_201_CREATED)
+            response = Response(department_serializer.data, status=status.HTTP_201_CREATED)
+            response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+            response["Access-Control-Max-Age"] = "1000"
+            response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+            return response
         else:
             print(department_serializer.errors)
             return Response(department_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
